eb22498f1d575ba8a05e8d01acdf11744100a827
Ralp Kendall <ralpkendall@gmail.com>
2022-10-31 17:47:25 +0800

    Add Multiplication Table

---
+++ 11-03-22/script.js
@@ -0,0 +1,40 @@
+//Model
+function getMultiplicandLimit() {
+    const multiplicandLimitInput = document.getElementById('multiplicand-limit');
+    return Number(multiplicandLimitInput.value);
+}
+
+function getMultiplier() {
+    const multiplierInput = document.getElementById('multiplier');
+    return Number(multiplierInput.value);
+}
+
+
+//View
+
+function renderResults() {
+
+}
+
+//Controller
+function process() {
+    const multiplicandLimit = getMultiplicandLimit();
+    const multiplier = getMultiplier();
+    
+    let productContainer = document.getElementById('product-container');
+    productContainer.a
+    productContainer.style.display = "block";
+
+    for(let i = 1;i <= multiplicandLimit;i++) {
+        let paragraph = document.createElement("p");
+        paragraph.innerText = multiply(i, multiplier);
+
+        productContainer.appendChild(paragraph);
+    }
+    
+}
+
+function multiply(multiplicand, multiplier) {
+    const product = multiplicand * multiplier;
+    return multiplicand + ' x ' + multiplier + ' = ' + product;
+}
